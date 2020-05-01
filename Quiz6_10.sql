bd880211c6e941044265f462edfee4922d186219
Shrividya KN <shrividya@Shrividyas-MacBook-Air.local>
2020-05-01 12:42:57 -0500

    Add quiz9 answers

--- Quiz6_10.sql
+++ Quiz6_10.sql
@@ -38,3 +38,16 @@
   SELECT *
   FROM web_events
   WHERE channel IN ('organic', 'adwords');
+
+/* 1.Use the accounts table to find the account name, primary poc, and sales rep id for all stores except Walmart, Target, 
+     and Nordstrom.
+   2.Use the web_events table to find all information regarding individuals who were contacted via any method except using 
+     organic or adwords methods. */
+
+  SELECT name, primary_poc, sales_rep_id
+  FROM accounts
+  WHERE name NOT IN ('Walmart', 'Target', 'Nordstrom');
+
+  SELECT *
+  FROM web_events
+  WHERE channel NOT IN ('organic', 'adwords');
