482b42525f05eb2174a12014cf1d64860c82cf2e
Shrividya KN <shrividya@Shrividyas-MacBook-Air.local>
2020-05-01 12:30:06 -0500

    Add quiz7 answers

--- Quiz6_10.sql
+++ Quiz6_10.sql
@@ -10,3 +10,19 @@
   SELECT id, account_id, poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd) AS post_per
   FROM orders
   LIMIT 10;
+
+/* 1.All the companies whose names start with 'C'.
+   2.All companies whose names contain the string 'one' somewhere in the name.
+   3.All companies whose names end with 's'.*/
+
+  SELECT name
+  FROM accounts
+  WHERE name LIKE 'C%' ;
+
+  SELECT name
+  FROM accounts
+  WHERE name LIKE '%one%' ;
+
+  SELECT name
+  FROM accounts
+  WHERE name LIKE '%s';
