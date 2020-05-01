c2bd4f9648bea7038bc6f6311dd096e97b7dd35d
Shrividya KN <shrividya@Shrividyas-MacBook-Air.local>
2020-05-01 12:16:47 -0500

    Add quiz3 answers

--- Quiz1_5.sql
+++ Quiz1_5.sql
@@ -24,3 +24,15 @@
   ORDER BY total_amt_usd 
   LIMIT 20;
 
+/* 1.Write a query that displays the order ID, account ID, and total dollar amount for all the orders, sorted first by the account
+    ID (in ascending order), and then by the total dollar amount (in descending order).
+   2.Now write a query that again displays order ID, account ID, and total dollar amount for each order, but this time sorted 
+    first by total dollar amount (in descending order), and then by account ID (in ascending order). */
+
+  SELECT id, account_id, total_amt_usd
+  FROM orders
+  ORDER BY account_id, total_amt_usd DESC;
+  
+  SELECT id, account_id, total_amt_usd
+  FROM orders
+  ORDER BY total_amt_usd DESC, account_id;
