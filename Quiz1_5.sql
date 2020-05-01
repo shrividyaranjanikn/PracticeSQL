b1a1f76ec4c8ceb08c3a924e044e5a40422cd65d
Shrividya KN <shrividya@Shrividyas-MacBook-Air.local>
2020-05-01 12:18:52 -0500

    Add quiz4 answers

--- Quiz1_5.sql
+++ Quiz1_5.sql
@@ -32,7 +32,21 @@
   SELECT id, account_id, total_amt_usd
   FROM orders
   ORDER BY account_id, total_amt_usd DESC;
-  
+
   SELECT id, account_id, total_amt_usd
   FROM orders
   ORDER BY total_amt_usd DESC, account_id;
+
+/* 1.Pulls the first 5 rows and all columns from the orders table that have a dollar amount of gloss_amt_usd greater than or
+    equal to 1000.
+   2.Pulls the first 10 rows and all columns from the orders table that have a total_amt_usd less than 500. */
+
+  SELECT * 
+  FROM orders
+  WHERE gloss_amt_usd >= 1000
+  LIMIT 5;
+
+  SELECT * 
+  FROM orders
+  WHERE total_amt_usd <= 500
+  LIMIT 10;
